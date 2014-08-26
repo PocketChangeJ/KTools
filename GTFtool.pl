2c97acef2046315ebb67b121f0aaba829ea01e1b
kentnf <kentnf@gmail.com>
2014-08-26 15:00:14 -0400

    GTFtool stat

--- GTFtool.pl
+++ GTFtool.pl
@@ -9,12 +9,71 @@ use IO::File;
 use Getopt::Std;
 
 my $version = 0.1;
+if (@ARGV < 1) { usage($version);}
+my $tool = shift @ARGV;
+
+my %options;
+getopts('i:o:p:fuh', \%options);
+
+if      ($tool eq 'stats')	{ stats(\%options); }
+elsif   ($tool eq 'convert') { convert(\%options); }
+else    { usage($version); }
+
+
+#################################################################
+# kentnf: subroutine						#
+#################################################################
+=head2
+ stat -- generate statistics information
+=cut
+sub stats
+{
+	my $options = shift;
+	my $subUsage = qq'
+USAGE: $0 stats [options]
+	-i      input file 
+
+';
+	print $subUsage and exit unless $$options{'i'};
+	my ($inFile, $out_prefix);
+	$inFile = $$options{'i'};
+
+	my %trans_info = parse_gtf($$options{'i'});
+
+	my %gene;
+	my %trans;
+	my %exon;
+	my $chr;
+	foreach my $tid (sort keys %trans_info)
+	{
+		$trans{$tid} = 1;
+		$gene{$trans_info{$tid}{'gid'}} = 1;
+		$chr = $trans_info{$tid}{'chr'};
+
+		my @exon = split(/\t/, $trans_info{$tid}{'exon'});
+		@exon = sort {$a<=>$b} @exon;
+
+		for(my $i=0; $i<scalar(@exon)-1; $i=$i+2)
+		{
+			my $start = $exon[$i];
+			my $end = $exon[$i+1];
+			my $key = "$chr\t$start\t$end";
+			$exon{$key} = 1;
+		}
+	}
+
+	print $inFile,"\tGene:",scalar(keys(%gene)),"\tTrans:",scalar(keys(%trans)),"\tExon:",scalar(keys(%exon)),"\n";
+
+}
 
 =head2
- convert 
+ convert -- convert GTF to GFF,BED,TAB format
 =cut
 sub convert
 {
+
+
+
 
 }
 
@@ -27,7 +86,7 @@ sub parse_gtf
 
 	my %trans_info; # key: tid, chr, exon, gene, strand
 	
-	my $fh = IO::File->new($input_gtf) || die $!;
+	my $fh = IO::File->new($input_file) || die $!;
 	while(<$fh>)
 	{
 		chomp;
@@ -45,14 +104,38 @@ sub parse_gtf
 				$attr_value{$c[0]} = $c[1];
 			}
 
-			
+			die "[ERR]No transcript id\n" unless defined $attr_value{'transcript_id'};
+			die "[ERR]No gene id\n" unless defined $attr_value{'gene_id'};
+			my ($tid, $gid) = ($attr_value{'transcript_id'}, $attr_value{'gene_id'});
 
+			if ( defined $trans_info{$tid}{'chr'} ) {
+				die "[ERR]inconsistency chr for $tid\n"	if $trans_info{$tid}{'chr'} ne $a[0];
+			} else {
+				$trans_info{$tid}{'chr'} = $a[0];
+			}
 
+			if ( defined $trans_info{$tid}{'gid'} ) {
+				die "[ERR]inconsistency gid for $tid\n" if $trans_info{$tid}{'gid'} ne $gid;
+			} else {
+				$trans_info{$tid}{'gid'} = $gid;
+			}
+
+			if ( defined $trans_info{$tid}{'strand'} ) {
+				die "[ERR]inconsistency strand for $tid\n" if $trans_info{$tid}{'strand'} ne $a[6];
+			} else {
+				$trans_info{$tid}{'strand'} = $a[6];
+			}
+
+			if ( defined $trans_info{$tid}{'exon'}) {
+				$trans_info{$tid}{'exon'}.="\t".$a[3]."\t".$a[4];
+			} else {
+				$trans_info{$tid}{'exon'} = $a[3]."\t".$a[4];
+			}
 		}
 	}
 	$fh->close;
 
-	return %gtf_info;
+	return %trans_info;
 }
 
 =head2
@@ -66,11 +149,11 @@ Program: GTFtools (Tools for GTF file)
 Version: $version
 
 USAGE: $0 <command> [options] 
-Command: 
+Command:
+	stats		statistics for GTF file 
         convert		convert GTF to BED/GFF/TAB format
 	import		import GFF to GTF (gffread)
 	extractList	extract GTF by list
-	stat		statistics for GTF file
 
 * the gtf file must have exon feature, transcript_id, and gene_id attributes
 
