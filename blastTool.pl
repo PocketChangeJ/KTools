394ba987a6c1d4a15870acbc9abcf8f0d780acf4
kentnf <kentnf@gmail.com>
2014-12-17 12:19:27 -0500

    blastTool.pl add function to remove duplication sequences

--- blastTool.pl
+++ blastTool.pl
@@ -8,6 +8,7 @@ use strict;
 use warnings;
 use FindBin;
 use IO::File;
+use Bio::SeqIO;
 use Bio::SearchIO;
 use Getopt::Std;
 
@@ -17,13 +18,96 @@ my %options;
 getopts('a:b:c:d:e:f:g:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:z:h', \%options);
 unless (defined $options{'t'} ) { usage($version); }
 
-if	($options{'t'} eq 'b2table') 	{ blast_to_table(\%options, \@ARGV); } 	# parse multi dataset
-elsif	($options{'t'} eq 'brh') 	{ blast_brh(\%options, \@ARGV); } 	# parse multi dataset
+if	($options{'t'} eq 'b2table') 	{ blast_to_table(\%options, \@ARGV); }
+elsif	($options{'t'} eq 'brh') 	{ blast_brh(\%options, \@ARGV); }
+elsif	($options{'t'} eq 'unique')	{ blast_unique(\%options, \@ARGV); }
 else	{ usage($version); }
 
 #================================================================
 # kentnf: subroutine						
 #================================================================
+=head
+ blast_unique: remove redundancy sequence
+=cut
+sub blast_unique
+{
+	my ($options, $files) = @_;
+	my $usage = qq'
+USAGE $0 -t unique input_seq blast_m8 > output
+
+* output file
+	1. input_seq.unique
+	2. input_seq.removed
+	3. input_seq.rmDup_table.txt
+
+* genrate unique seqs according self-blast m8 output
+* only works for %100 identify and %100 alignment
+
+
+
+';
+	print $usage and exit unless (scalar @$files == 2);
+	my ($input_seq, $blast_m8) = @$files;
+
+	# put seq_info to hash
+	my %seq_info;
+	my $in = Bio::SeqIO->new(-format=>'fasta', -file=>$input_seq);
+	while(my $inseq = $in->next_seq)
+	{
+		$seq_info{$inseq->id}{'seq'} = $inseq->seq;
+		$seq_info{$inseq->id}{'len'} = $inseq->length;
+	}
+
+	# put removed seq to hash
+	my %removed;
+	my $fh = IO::File->new($blast_m8) || die $!;
+	while(<$fh>)
+	{
+		chomp;
+		next if $_ =~ m/^#/;
+		my @a = split(/\t/, $_);
+		# Qid, Sid, %iden, alignLen, mismatches, gap open, q.start, q.end, s.start, s.end, evalue, score
+		die "[ERR]line:$_\n" unless @a == 12;
+		my ($qid, $sid, $iden, $alen, $mismatch, $gap, $q_start, $q_end, $s_start, $s_end, $evalue, $score) = @a;
+		my $qlen = $seq_info{$qid}{'len'};
+		my $slen = $seq_info{$sid}{'len'};
+		next if $qid eq $sid;
+		next if ($iden < 100 || $mismatch > 0 || $gap > 0);
+		next if ($alen < $qlen && $alen < $slen);
+
+		if  ( $alen == $qlen && $q_start == 1 && $q_end == $qlen) {
+			$removed{$qid} = $sid;
+		}
+
+		if  ( $alen == $slen && $s_start == 1 && $s_end == $slen ) {
+                        $removed{$sid} = $qid;
+                }
+
+		if  ( $alen == $slen && $s_start == $slen && $s_end == 1 ) {
+			$removed{$sid} = $qid;
+		}
+	}
+	$fh->close;
+	
+	# output unique 
+	my $out1 = IO::File->new(">".$input_seq.".removed") || die $!;
+	my $out2 = IO::File->new(">".$input_seq.".unique") || die $!;
+	my $out3 = IO::File->new(">".$input_seq.".rmDup_table.txt") || die $!;
+
+	foreach my $id (sort keys %seq_info) {
+		if (defined $removed{$id} ) {
+			print $out1 ">".$id."\n".$seq_info{$id}{'seq'}."\n";
+			print $out3 $id."\t".$removed{$id}."\n";
+		} else {
+			print $out2 ">".$id."\n".$seq_info{$id}{'seq'}."\n";
+		}
+	}
+
+	$out1->close;
+	$out2->close;
+	$out3->close;
+}
+
 =head
  blast_brh: find best reciprocal hits in blast result
 =cut
@@ -169,6 +253,7 @@ USAGE: $0 -t [tools]
 
 	b2table		convert blast result to table
 	brh		best reciprocal hit
+	unique		remove redundancy using self-blast m8 result
 
 ';
 	print $usage; 
