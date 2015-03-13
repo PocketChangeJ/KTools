e65d97bb9119e90c171270fed80936968b318b79
kentnf <kentnf@gmail.com>
2015-03-13 11:58:32 +0000

    add dbtool for parse database, init it for get EC code

---
+++ dbtool.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+=head
+ dbtool.pl -- parse database for in house use 
+=cut
+use strict;
+use warnings;
+
+
+
+
+
+
+my $usage = qq'
+USAGE: $0 input_database(dat format)
+
+';
+
+my $database = shift || die $usage;
+
+my $db = 'sp';
+$db = 'tr' if $database =~ m/trembl/ig;
+uniport2id($database, $db);
+
+#################################################################
+# kentnf: subroutine						#
+#################################################################
+
+=head2
+ convert uniport to fasta 
+=cut
+sub uniport2id
+{
+	my ($input, $db) = @_;
+	my $output = $input.".id.txt";
+
+	my ($id, $ac, @ec);
+	open(OUT, ">".$output) || die $!;
+	open(FH, $input) || die $!;
+	while(<FH>) {
+		chomp;
+		if	(/^ID\s+(\S+)/)		{ $id = $1; }
+		elsif	(/^AC\s+(\S+)/)		{ $ac = $1; $ac =~ s/;//ig; }
+		elsif	(/^DE\s+EC=(\S+)/)	{ my $ec = $1; $ec =~ s/;//ig; push(@ec, $ec); } 
+		elsif	(/^\/\//) {
+			die "[ERR]undef ID or ACC\n" unless ($id && $ac);
+			my $ec_all = '';
+			$ec_all = join(";", @ec) if (scalar @ec > 0);
+			print $ec_all."\t$id|$ac\n" if (scalar @ec > 1);
+			print OUT "$db|$ac|$id\t$ec_all\n";	
+			$ac = ''; $id = ''; @ec = ();
+		}
+		else	{ next; }	
+	}
+	close(FH);
+	close(OUT);
+}
+
